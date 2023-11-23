4f28077dc08be2009d8c82b70bd2c62609e0dd78
=Arvind Kiwelekar <=awk@dbatu.ac.in>
2023-11-23 06:39:33 +0530

    linted

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,34 @@
+"""Module providing a calculator function printing python version."""
+
+
 def add(x,y):
+    """Function printing python version."""
     return x+y
 
 def substract(x, y):
+    """Function printing python version."""
     return x-y
 
 def multiply(x, y):
+    """Function printing python version."""
     return x*y
 
 def divide(x,y):
+    """Function printing python version."""
     return x/y
 
-def max(x,y,z):
- m = x
- if(y>m):
-  m=y
- if(z>m):
-  m=z
- return m 
+def max_1(x,y,z):
+    """Function printing python version."""
+    m = x
+    if y>m :
+        m=y
+    if z>m :
+        m=z
+    return m
+
+
+print("Sum of 10 and 5 is===   ",  add(10,5))
+print("Substraction of 10 and 5 is===   ",  substract(10,5))
+print("multiplication of 10 and 5 is===   ",  multiply(10,5))
+print("division of of 10 and 5 is===   ",  divide(10,5))
+print("max of 17, 10 and 5 is ===   ",  max_1(17,10,5))
